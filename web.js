aabc58b0dbed8dbca89d112283a47b35f625448a
Jorge Carrillo <jece2k@gmail.com>
2013-07-08 05:10:01 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,11 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  var xyesbuffer = fs.readFileSync('index.html', "utf8");
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
